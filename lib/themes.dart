d8ed580314b3cccabdd9aebecb944b6a7b953031
Zain Haider Naqvi <syedzainnaqvi3324@gmail.com>
2022-07-29 23:33:17 +0500

    Added themes to the application

---
+++ lib/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  // light theme
+  static final light = ThemeData(
+    primaryColor: Colors.blue,
+    brightness: Brightness.light,
+  );
+  // dark theme
+  static final dark = ThemeData(
+    primaryColor: Colors.yellow,
+    brightness: Brightness.dark,
+  );
+}
